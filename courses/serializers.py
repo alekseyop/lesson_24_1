85249294305751491137c8e90447a5b63c14b3ee
Aleksey <oszn2011@yandex.ru>
2024-09-15 00:04:17 +0300

    add load_payments

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,8 +3,18 @@ from rest_framework.serializers import ModelSerializer
 from courses.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ["id", "title", "description", "preview", "video_url"]
+
+
 class CourseSerializer(ModelSerializer):
     lessons_count = serializers.SerializerMethodField()  # Счетчик уроков
+
+    # Поле для вывода информации обо всех уроках
+    # many=True - указываем, что мы хотим получить список уроков
+    lessons = LessonSerializer(many=True, read_only=True)
 
     # Метод для подсчета уроков
     def get_lessons_count(self, obj):
@@ -19,10 +29,5 @@ class Meta:
         "preview",
         "description",
         "lessons_count",
+        "lessons",
     ]  # Указываем новое поле в fields
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = ["id", "title", "description", "preview", "video_url", "course"]
